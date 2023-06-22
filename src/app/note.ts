56dfc92e7d84e7ae90e5a30342c4d54e0b49984b
Onkar Jadhav <omjadhav2610@gmail.com>
2023-06-22 23:13:51 +0530

    safety

---
+++ src/app/note.ts
@@ -0,0 +1,9 @@
+export interface Note {
+  title : string
+  body : string
+  // background : string | null
+  // labels : string[]
+  // images : string[]
+  // showCheckboxes : boolean
+  // editedTime: string
+}
---
+++ src/app/note/note.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {Note} from "../note";
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent {
+  @Input() note! : Note
+
+// background : string | null
+  // labels : string[]
+  // images : string[]
+  // showCheckboxes : boolean
+  // editedTime: string
+}
